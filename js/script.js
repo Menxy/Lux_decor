6885e8ab79ebd07f790683300c9eb25d66f8ef82
Дюша <58334576+Menxy@users.noreply.github.com>
2020-05-09 15:22:40 +0300

    slick 2

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
     $('.big__slider').slick({
         arrows: true,
         dots: true,//положение меняется стилизацией контейнера .slick-dots
-        adaptiveHeight: true, // если true для slick-track align-items: flex-start;
+        adaptiveHeight: false, // если true для slick-track align-items: flex-start;
         //slidesToShow: 1, //количество отображаемых слайдов
         // slidesToScroll: 10, //количество слайдов перемотки
         // speed: 1000, // 300 умолчание в ms
         // easing: "ease", //как в анимации css
-        infinite: true, // бесконечная перемотка (true)
+        // infinite: true, // бесконечная перемотка (true)
         // initialSlide: 2, //стартовый слайд
         // autoplay: true,  
         // autoplaySpeed: 2000, //ms, к autoplay
@@ -17,6 +17,13 @@ $(document).ready(function(){
         centerMode: false, //активный слайд по центру. Показывает 3 слайда. Получает класс .slick-center
         variableWidth: false, // сбрасывает width. Хорошо работает с centerMode, плохо со slidesToShow
         //appendArrows: $('.main__slider__container'),
+        //respondTo: slider, // width к window, slider, min из двух
+        //rtl: true, //right to left
     });
+    $('.hits__slider').slick({
+        arrows: true,
+        dots: true,//положение меняется стилизацией контейнера .slick-dots
+        slidesToShow: 4, //количество отображаемых слайдов
 
+    });
 })
